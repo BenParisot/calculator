5abcf89ce18388384e2c46477ef065bfba1856f3
Ben Parisot and Matt Jackson <ben@>
2017-10-04 20:13:22 -0700

    add BMI calculator script to calculator.js

--- js/calculator.js
+++ js/calculator.js
@@ -6,3 +6,39 @@ var number1 = parseInt(prompt("Enter a number: "));
 var number2 = parseInt(prompt("Enter another number: :"));
 
 alert(add(number1, number2));
+
+var subtract = function(number1, number2) {
+	return number1 - number2;
+};
+
+var number1 = parseInt(prompt("Enter a number: "));
+var number2 = parseInt(prompt("Enter another number: :"));
+
+alert(subtract(number1, number2));
+
+var divide = function(number1, number2) {
+	return number1 / number2;
+};
+
+var number1 = parseInt(prompt("Enter a number: "));
+var number2 = parseInt(prompt("Enter another number: :"));
+
+alert(divide(number1, number2));
+
+var multiply = function(number1, number2) {
+	return number1 * number2;
+};
+
+var number1 = parseInt(prompt("Enter a number: "));
+var number2 = parseInt(prompt("Enter another number: :"));
+
+alert(multiply(number1, number2));
+
+var mass = parseInt(prompt("What is your weight?"));
+var heightSquare = parseInt(prompt("What is your height"));
+
+var BMI = function(mass, heightSquare) {
+	return mass / (heightSquare * heightSquare);
+};
+
+alert(BMI(mass, heightSquare));
